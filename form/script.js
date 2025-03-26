b8b8ba56f4d557a5e44e0f73ef0039c3add039d0
EwertonWeb <ewerton.rl@hotmail.com>
2025-03-25 23:08:14 -0300

    Estruturando o validador e Exibindo o erro

---
+++ form/script.js
@@ -0,0 +1,73 @@
+let b7validator = {
+    handlesubmit:(Event)=>{
+        Event.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        b7validator.clearErrors();
+
+        for(let i=0;i<inputs.length;i++) {
+            let input = inputs[i];
+            let check = b7validator.chekinput(input);
+            if(check !== true) {
+                send = false;
+                b7validator.showError(input, check);
+                
+            }
+        }
+
+        if(send){
+            form.submit();
+
+        }
+    },
+    chekinput:(input) => {
+        let rules = input.getAttribute('data-rules');
+        
+        if(rules !== null ) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rDatails = rules[k].split('=');
+                switch(rDatails[0]){
+                    case 'required':
+                        if(input.value == ''){
+                            return 'campo nao pode ser vazio.';
+                        }
+                    break;
+                    case'min':
+
+                    break;
+                }
+            }
+
+        }
+        return true;
+    },
+    showError:(input , error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+
+    },
+    clearErrors:() => {
+        let inputs =form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++){
+            inputs[i].style = '';
+        }
+
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
+    }
+};
+
+
+let form =document.querySelector('.b7validator');
+form.addEventListener('submit', b7validator.handlesubmit);
